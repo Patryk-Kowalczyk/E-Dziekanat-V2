21fd40be06a8ce05e35540c711863c0131f2f3ab
OsmanK98 <osmankrzysztof123@gmail.com>
2020-11-27 12:53:30 +0100

    Druga wersja

---
+++ backend/app/Http/Controllers/Test.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $tab = [[1,2,3,4],[5,3,2,5]];
+
+        //return view('home');
+    }
+}
