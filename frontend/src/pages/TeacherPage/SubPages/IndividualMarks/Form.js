5f388eba9e43918498db740b8842eaea2533c254
Marcin Wrzos <marwrz36@gmail.com>
2021-01-19 14:55:29 +0100

    onChange value fixed in marksForm

--- frontend/src/pages/TeacherPage/SubPages/IndividualMarks/Form.js
+++ frontend/src/pages/TeacherPage/SubPages/IndividualMarks/Form.js
@@ -1,9 +1,28 @@
 import React, {useState} from 'react';
+import Input from "./Input";
 
 const Form = ({element}) => {
     const [isOpenForm, setOpenForm] = useState(false);
     const [formMarks, formMarksUpdate] = useState(element.marks);
 
+    const [isVisible, setIsVisible] = useState(false);
+    const [visibleValueDesc, setVisibleValueDesc] = useState('');
+    const [visibleValueMark, setVisibleValueMark] = useState('');
+
+
+    const handleOnChangeDescription = (e) => {
+        const edit = [...formMarks];
+        edit[e.target.placeholder].description = e.target.value;
+        formMarksUpdate(edit)
+    }
+    const handleOnChangeMark = (e) => {
+        const edit = [...formMarks];
+        edit[e.target.placeholder].mark = e.target.value;
+        formMarksUpdate(edit)
+    }
+    const handleOnClick = (e) => {
+
+    }
     return (
         <>
             <div key={element.unique_number} className="nameLastName" onClick={() => setOpenForm(!isOpenForm)}>
@@ -14,18 +33,31 @@ const Form = ({element}) => {
                     <form>
                         {formMarks.map((e, i) => {
                             return (
-                                // <div style={{paddingLeft: "10px"}} className="mark" key={e.id}>
-                                //     <div className="mark2">{e.mark}</div>
-                                // </div>
-                                <>
-                                    <div>
-                                        <input className="inputForm" type="text" value={e.description}/>
-                                        <input className="inputForm" type="text" value={e.mark}/>
-                                        <br/>
-                                    </div>
-                                </>
+                                <div>
+                                    <Input valueI={i}
+                                           valueInput={formMarks[i].description}
+                                           handleOnChange={handleOnChangeDescription}/>
+                                    <Input valueI={i}
+                                           valueInput={formMarks[i].mark}
+                                           handleOnChange={handleOnChangeMark}/>
+                                    <br/>
+                                </div>
                             )
                         })}
+                        {isVisible === true ? (
+                                <>
+                                    <input type="text" className="inputForm" value={visibleValueDesc}
+                                           onChange={setVisibleValueDesc}
+                                           placeholder={"Kategoria"}/>
+                                    <input type="text" className="inputForm" value={visibleValueMark}
+                                           onChange={setVisibleValueMark}
+                                           placeholder={"Ocena"}
+                                    />
+
+                                </>
+                            )
+                            : null}
+                        <div className="divPlus" onClick={() => setIsVisible(!isVisible)}>+</div>
                         <button className="buttonForm">Submit</button>
                     </form>
                 </div>
--- frontend/src/pages/TeacherPage/SubPages/IndividualMarks/SelectedTable.js
+++ frontend/src/pages/TeacherPage/SubPages/IndividualMarks/SelectedTable.js
@@ -12,24 +12,7 @@ const SelectedTable = ({selected}) => {
             return (
                 item.squad.map((element, i) => {
                         return (
-                            // <>
-                            //     <div key={element.unique_number} className="nameLastName">
-                            //         {element.first_name}
-                            //         {" "}
-                            //         {element.last_name}
-                            //         <div style={{paddingLeft: "10px"}}>{"Oceny: "}</div>
-                            //         {element.marks.map((e, i) => {
-                            //             return (
-                            //                 <div style={{paddingLeft: "10px"}} className="mark" key={e.id}>
-                            //                     <div className="mark2">{e.mark}</div>
-                            //                 </div>)
-                            //         })}
-                            //         <button>Edytuj</button>
-                            //     </div>
-                            // </>
-                            //    ZROBIE TYLKO OSOBY
-                            <Form key={element.first_name + element.uniqueID} element={element}/>
-
+                            <Form key={element.unique_number} element={element}/>
                         )
                     }
                 ))
